57ac62bf50514aa23b46e8506f36ebbc7ce717df
kvb <kumarivb@gmail.com>
2018-03-17 12:59:15 -0500

    got rid of config.json error

--- models/index.js
+++ models/index.js
@@ -8,7 +8,11 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/..\config\config.json')[env];
+// var config    = require(__dirname + '../../config\config.json')[env];
+console.log("dirname=" + __dirname);
+console.log(__dirname + "/../config/config.json");
+var config    = require(__dirname + "/../config/config.json")[env];
+
 var db        = {};
 
 if (config.use_env_variable) {
