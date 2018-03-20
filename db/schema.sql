a5b147cdb30d27dbfddb540e9eb139ba94095bf8
Jeff DuBois <jeff.adubois@gmail.com>
2018-03-19 20:46:19 -0500

    add definitions table

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,26 @@
 CREATE DATABASE definethis_db;
+USE  definethis_db;
 
 CREATE TABLE Entries (
   id INTEGER AUTO_INCREMENT,
-  entry VARCHAR(50),
+  entry VARCHAR(50) NOT NULL,
+  createdAt DATETIME,
+  updatedAt DATETIME,
+  userId INT NOT NULL,
+  context VARCHAR(420),
+  contextLink VARCHAR(200),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Definitions (
+  id INTEGER AUTO_INCREMENT,
+  definition VARCHAR(420) NOT NULL,
   createdAt DATETIME,
   updatedAt DATETIME,
   userId INT,
+  entryId INT NOT NULL,
+  netRating INT NOT NULL,
   context VARCHAR(420),
-  contextLink VARCHAR(200)
+  contextLink VARCHAR(200),
+  PRIMARY KEY (id)
 );
-
-   
-
-
