b049082d573afe15990a446d7ea4c59916c2f7f7
EmrenIng <Ingvoldstad.Maya@gmail.com>
2018-03-19 19:34:55 -0500

    Control Up

--- controllers/controller.js
+++ controllers/controller.js
@@ -8,6 +8,33 @@ var router = express.Router();
 
 // routes
     // create routes here
+    // get route -> index, Sets the Main Server Page to Index
+    router.get("/", function(req, res) {
+        res.redirect("/index");
+    });
+
+    router.get("/cards", function(req, res) {
+        // replace old function with sequelize function
+        definethis_db.findAll({
+            include: [db.Customer],
+            // Here we specify we want to return our burgers in ordered by ascending burger_name
+            order: [
+                ["burger_name", "ASC"]
+            ]
+        })
+            // use promise method to pass the burger
+            .then(function(dbBurger) {
+            // into the main index, updating the page
+            // var hbsObject = {
+            //     burger: dbBurger
+            // };
+            return res.render("index", hbsObject);
+            });
+        });
+
+
 
 // export router
 module.exports = router;
+
+
