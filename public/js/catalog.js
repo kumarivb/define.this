2216509c017f7033b0ec0a07d94122fc4fd053e4
kvb <kumarivb@gmail.com>
2018-03-21 21:41:32 -0500

    pull from database

---
+++ public/js/catalog.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // card container
+    var cardContainer = $(".indexCard");
+});
+
+// grab context info from db
+getContext();
+
+// get context
+function getContext() {
+    $.post("/api/getfirstentry", function(data) {
+        context = data;
+        console.log(data);
+    });
+}
